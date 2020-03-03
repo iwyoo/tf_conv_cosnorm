17c600f0cececd328b5f0beae8a152358d62f5fe
Henry Eigen <eigen.henry@gmail.com>
2020-03-03 13:01:45 -0500

    initial commit

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ Tensorflow implementation of cosine normalization [1] for convolutional layer.
 There is no implementation detail of convolution version in the original paper [1].
 Therefore, the performance of this implementation is not guaranteed.
 
-## Usage
+This fork adds a keras layer wrapper for iwyoo's TF implementation. 
+
+## Usage (Tensorflow)
 ```
 # Example of using tf.nn.conv2d
 conv = tf.nn.conv2d(x, w, strides, padding)
@@ -14,9 +16,21 @@ conv = conv2d_cosnorm(x, w, strides, padding)
 relu = tf.nn.relu(conv) # No bias needed
 ```
 
+## Usage (Keras)
+```
+# Keras Conv2d
+from keras.layers import Conv2d
+model.add(Conv2d(64, (3, 3), strides=(1, 1)))
+
+# Norm_Conv2d
+from sim_layer import Norm_Conv2d
+model.add(Norm_Conv2d(64, (3, 3), strides=(1, 1)))
+```
+
 ## Test
 A modified version of test code [2] is used to dubug and test of this implementation.
 
 ## References
 - [1] Chunjie, Luo, and Yang Qiang. "Cosine Normalization: Using Cosine Similarity Instead of Dot Product in Neural Networks." (https://arxiv.org/abs/1702.05870)
 - [2] https://github.com/aymericdamien/TensorFlow-Examples
+- [3] https://github.com/keras-team/keras/blob/master/keras/layers/convolutional.py
---
+++ sim_layer.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+"""
+Convolutional layers.
+"""
+
+from conv_cosnorm import conv2d_cosnorm
+
+from keras import backend as K
+from keras import activations
+from keras import initializers
+from keras import regularizers
+from keras import constraints
+from keras.engine.base_layer import Layer
+from keras.engine.base_layer import InputSpec
+from keras.utils import conv_utils
+from keras.utils.generic_utils import transpose_shape
+from keras.legacy import interfaces
+
+
+# ------------------------------------------------------------------------
+
+
+def N_conv(x, kernel, strides=(1, 1), padding='valid',
+           data_format=None, dilation_rate=(1, 1)):
+
+    data_format = normalize_data_format(data_format)
+
+    x, tf_data_format = _preprocess_conv2d_input(x, data_format)
+
+    padding = _preprocess_padding(padding)
+
+    # TF 2 arg conversion
+    kwargs = {}
+    if _is_tf_1():
+        kwargs['dilation_rate'] = dilation_rate
+    else:
+        kwargs['dilations'] = dilation_rate
+
+    x = conv2d_cosnorm(
+        x, kernel,
+        strides=strides,
+        padding=padding)
+    
+    if data_format == 'channels_first' and tf_data_format == 'NHWC':
+        x = tf.transpose(x, (0, 3, 1, 2))  # NHWC -> NCHW
+    return x
+
+# ------------------------------------------------------------------------   
+
+
+class _Conv(Layer):
+
+    def __init__(self, rank,
+                 filters,
+                 kernel_size,
+                 strides=1,
+                 padding='valid',
+                 data_format=None,
+                 dilation_rate=1,
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
+        super(_Conv, self).__init__(**kwargs)
+        self.rank = rank
+        self.filters = filters
+        self.kernel_size = conv_utils.normalize_tuple(kernel_size, rank,
+                                                      'kernel_size')
+        self.strides = conv_utils.normalize_tuple(strides, rank, 'strides')
+        self.padding = conv_utils.normalize_padding(padding)
+        self.data_format = K.normalize_data_format(data_format)
+        self.dilation_rate = conv_utils.normalize_tuple(dilation_rate, rank,
+                                                        'dilation_rate')
+        self.activation = activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+        self.input_spec = InputSpec(ndim=self.rank + 2)
+
+    def build(self, input_shape):
+        if self.data_format == 'channels_first':
+            channel_axis = 1
+        else:
+            channel_axis = -1
+        if input_shape[channel_axis] is None:
+            raise ValueError('The channel dimension of the inputs '
+                             'should be defined. Found `None`.')
+        input_dim = input_shape[channel_axis]
+        kernel_shape = self.kernel_size + (input_dim, self.filters)
+
+        self.kernel = self.add_weight(shape=kernel_shape,
+                                      initializer=self.kernel_initializer,
+                                      name='kernel',
+                                      regularizer=self.kernel_regularizer,
+                                      constraint=self.kernel_constraint)
+        if self.use_bias:
+            self.bias = self.add_weight(shape=(self.filters,),
+                                        initializer=self.bias_initializer,
+                                        name='bias',
+                                        regularizer=self.bias_regularizer,
+                                        constraint=self.bias_constraint)
+        else:
+            self.bias = None
+        # Set input spec.
+        self.input_spec = InputSpec(ndim=self.rank + 2,
+                                    axes={channel_axis: input_dim})
+        self.built = True
+
+    def call(self, inputs):
+        
+        outputs = N_conv(
+            inputs,
+            self.kernel,
+            strides=self.strides,
+            padding=self.padding)
+
+
+        if self.use_bias:
+            outputs = K.bias_add(
+                outputs,
+                self.bias,
+                data_format=self.data_format)
+
+        if self.activation is not None:
+            return self.activation(outputs)
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_last':
+            space = input_shape[1:-1]
+        elif self.data_format == 'channels_first':
+            space = input_shape[2:]
+        new_space = []
+        for i in range(len(space)):
+            new_dim = conv_utils.conv_output_length(
+                space[i],
+                self.kernel_size[i],
+                padding=self.padding,
+                stride=self.strides[i],
+                dilation=self.dilation_rate[i])
+            new_space.append(new_dim)
+        if self.data_format == 'channels_last':
+            return (input_shape[0],) + tuple(new_space) + (self.filters,)
+        elif self.data_format == 'channels_first':
+            return (input_shape[0], self.filters) + tuple(new_space)
+
+    def get_config(self):
+        config = {
+            'rank': self.rank,
+            'filters': self.filters,
+            'kernel_size': self.kernel_size,
+            'strides': self.strides,
+            'padding': self.padding,
+            'data_format': self.data_format,
+            'dilation_rate': self.dilation_rate,
+            'activation': activations.serialize(self.activation),
+            'use_bias': self.use_bias,
+            'kernel_initializer': initializers.serialize(self.kernel_initializer),
+            'bias_initializer': initializers.serialize(self.bias_initializer),
+            'kernel_regularizer': regularizers.serialize(self.kernel_regularizer),
+            'bias_regularizer': regularizers.serialize(self.bias_regularizer),
+            'activity_regularizer':
+                regularizers.serialize(self.activity_regularizer),
+            'kernel_constraint': constraints.serialize(self.kernel_constraint),
+            'bias_constraint': constraints.serialize(self.bias_constraint)
+        }
+        base_config = super(_Conv, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+    
+# ------------------------------------------------------------------------
+
+
+
+# ------------------------------------------------------------------------   
+    
+
+class Norm_Conv2d(_Conv):
+    def __init__(self, filters,
+                 kernel_size,
+                 strides=(1, 1),
+                 padding='valid',
+                 data_format=None,
+                 dilation_rate=(1, 1),
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
+        super(Norm_Conv2d, self).__init__(
+            rank=2,
+            filters=filters,
+            kernel_size=kernel_size,
+            strides=strides,
+            padding=padding,
+            data_format=data_format,
+            dilation_rate=dilation_rate,
+            activation=activation,
+            use_bias=use_bias,
+            kernel_initializer=kernel_initializer,
+            bias_initializer=bias_initializer,
+            kernel_regularizer=kernel_regularizer,
+            bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer,
+            kernel_constraint=kernel_constraint,
+            bias_constraint=bias_constraint,
+            **kwargs)
+
+    def get_config(self):
+        config = super(Norm_Conv2d, self).get_config()
+        config.pop('rank')
+        return config
+
